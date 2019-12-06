7678aced26417ace2a225826b42a90ae1c9fd519
sliptype <sliptype@gmail.com>
2019-12-05 20:23:26 -0600

    Update readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Polymorphism in TypeScript`,
+    description: `A presentation for the Dallas React Meetup`,
+    author: `@sliptype`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
